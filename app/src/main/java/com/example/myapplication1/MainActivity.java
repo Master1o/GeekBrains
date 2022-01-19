09b32abf6a64b9b9d35c893e2cd922e451b429b1
Vitaly Krivonos <vitlik_krivonos@mail.ru>
2022-01-19 19:49:21 +0300

    Практическое задание №1

---
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.myapplication1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText inputOne = findViewById(R.id.input_one);
+        EditText inputTwo = findViewById(R.id.input_two);
+
+        Button compare = findViewById(R.id.btn_compare);
+        TextView result = findViewById(R.id.result);
+
+        compare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            Integer one = Integer.valueOf(inputOne.getText().toString());
+            Integer two = Integer.valueOf(inputTwo.getText().toString());
+
+            if (one == two) {
+                result.setText("Equal");
+            } else {
+                result.setText("Not Equal");
+            }
+
+            }
+        });
+    }
+}
